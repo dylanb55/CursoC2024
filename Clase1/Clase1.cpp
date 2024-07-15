7fd4407050e7e22ff96b8c0c45f59ad6744ab6cb
dylanb55 <161396341+dylanb55@users.noreply.github.com>
2024-07-15 14:57:57 -0400

    Add files via upload

---
+++ Clase1/Clase1.cpp
@@ -0,0 +1,10 @@
+#include<iostream> //Libreria de entrada y salida de datos de c++
+
+using namespace std; //Para simplificar la escritura de codigo
+
+int main(){ //Funcion principal
+	cout << "Hola mundo" << endl; 
+	cout << "Esta es la primera clase" << endl;
+	
+	
+}
